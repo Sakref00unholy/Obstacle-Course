aabacdffc63515285da930c322ce3583802b124d
Sakref00unholy <phillipmayland@gmail.com>
2023-01-16 17:13:34 +0100

    First Commit
    full game with almost all functionalities.

---
+++ Assets/Scripts/Scorer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scorer : MonoBehaviour
+{
+    float hits = -1f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Hit")
+        {
+            hits++;
+            Debug.Log("You have bumped into things this amount of times: " + hits);
+        }
+    }
+}
